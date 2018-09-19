8b23c9d7f1257ce9511ec5acbf0fc0e03587df6a
ssjrnath <ssjrnath1@gmail.com>
2018-09-20 00:05:47 +0530

    Attempting to connect to db multiple times
    Co-Authored-By: veera venky  <veeravenky222@gmail.com>

--- app.js
+++ app.js
@@ -9,26 +9,38 @@ const dataHandler = require('./src/routes/dataHandler.js');
 
 url = 'mongodb://localhost:27017';
 dbName = 'chitragupt';
+dbRetries = config.db.retries || 3;
 
-MongoClient.connect(url,function(err,client){
-  if(err){
-    console.log("Not able to connect db");
-    process.exit(2);
-  }
-  console.log("Successfully connected to DB");
-  client.close();
-});
+function connectDb() {
+  MongoClient.connect(url, function(err, client) {
+      if (err) {
+        if (dbRetries) {
+          console.log(`Not able to connect db, retrying... `);
+          --dbRetries;
+          connectDb();
+          return;
+        } else {
+          console.log("Not able to connect db, exiting");
+          process.exit(2);
+      }
+    }
+    console.log("Successfully connected to DB"); client.close();
+  });
+};
+connectDb();
 
 const app = express();
 
-app.listen(config.port,()=>console.log(`Magic happens at ${config.port}`));
+app.listen(config.port, () => console.log(`Magic happens at ${config.port}`));
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(express.json());
 
 app.use(morgan('dev'));
 
-app.get('/',(req,res)=>res.send("Hello don't worry I am up "));
-app.get('/schemas/:name',schemaHandler.getSchema);
+app.get('/', (req, res) => res.send("Hello don't worry I am up "));
+app.get('/schemas/:name', schemaHandler.getSchema);
 
-app.put('/schemas/:name',schemaHandler.addSchema);
+app.put('/schemas/:name', schemaHandler.addSchema);
